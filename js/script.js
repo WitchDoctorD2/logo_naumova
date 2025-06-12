014be81daa56dba1afb2cc8d460d3caebb50f01a
Uchicha Itachi <dcernys188@gmail.com>
2025-06-12 20:17:39 +0200

    add

--- js/script.js
+++ js/script.js
@@ -138,3 +138,12 @@ window.addEventListener('resize', updateCarousel);
 
 // Инициализация
 updateCarousel();
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('.header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
